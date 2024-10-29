fb1f3da7adf00ba8914d8dc28f0c410df9198030
Szymon <50032467+SBaluszek@users.noreply.github.com>
2024-10-29 12:57:47 +0100

    Update AlignSTARdbGAP.R

--- AlignSTARdbGAP.R
+++ AlignSTARdbGAP.R
@@ -1,16 +1,16 @@
 library(data.table)
 library(dplyr)
 #library(parallel)
-x = unique(gsub("_1.fq.gz", "", gsub("_2.fq.gz", "", list.files("/mnt/wspolny/sbaluszek/pitnet/fastq"))))
+x = unique(gsub("_1.fq.gz", "", gsub("_2.fq.gz", "", list.files("/mnt/ext/sbaluszek/pitnet/fastq"))))
 
 cmd = c(
   "STAR",
   "--runThreadN 48",
   "--runMode alignReads",
   "--readFilesCommand zcat",
-  "--genomeDir /mnt/wspolny/sbaluszek/genomes/mouseSTAR",
-  "--readFilesIn /mnt/wspolny/sbaluszek/pitnet/fastq/%s_1.fq.gz /mnt/wspolny/sbaluszek/pitnet/fastq/%s_2.fq.gz",
-  "--outFileNamePrefix /mnt/wspolny/sbaluszek/pitnet/aligned/%s/%s_",
+  "--genomeDir /mnt/ext/sbaluszek/genomes/mouseSTAR",
+  "--readFilesIn /mnt/ext/sbaluszek/pitnet/fastq/%s_1.fq.gz /mnt/ext/sbaluszek/pitnet/fastq/%s_2.fq.gz",
+  "--outFileNamePrefix /mnt/ext/sbaluszek/pitnet/aligned/%s/%s_",
   "--outSAMtype BAM SortedByCoordinate",
   "--chimSegmentMin 20",
   "--chimOutType Junctions",
@@ -19,7 +19,7 @@ cmd = c(
 cmd = paste(cmd, collapse = " ")
 
 for(ex in x){
-  targetDir = sprintf("/mnt/wspolny/sbaluszek/aligned/%s", ex)
+  targetDir = sprintf("/mnt/ext/sbaluszek/aligned/%s", ex)
 tm = Sys.time()
     system(sprintf("mkdir %s", targetDir))
     system(sprintf(cmd, ex, ex, ex, ex))
